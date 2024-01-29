b11d9b90161705fc160a99c91db2b045d8c7fccc
a.agayan95@mail.ru <a.agayan95@mail.ru>
2024-01-29 14:23:08 +0400

    add test

--- com/example/main/Person.java
+++ com/example/main/Person.java
@@ -15,4 +15,10 @@ public class Person {
                 ", age=" + age +
                 '}';
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return name.equals(((Person) o).name);
+    }
 }
